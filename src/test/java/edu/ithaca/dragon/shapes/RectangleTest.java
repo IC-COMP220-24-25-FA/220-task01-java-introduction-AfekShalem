2e3317adda1ac72a13f7ef1cf1a01fb80576a892
afeks <ashalemm@ithaca.edu>
2024-08-28 18:47:47 -0400

    longestLineWithin tests implemented

--- src/test/java/edu/ithaca/dragon/shapes/RectangleTest.java
+++ src/test/java/edu/ithaca/dragon/shapes/RectangleTest.java
@@ -16,4 +16,15 @@ public class RectangleTest {
         myRectangle = new Rectangle(2,0);
         assertEquals(0, myRectangle.calcArea(), 0.0001);
     }
+    @Test
+    public void longestLineWithinTest(){
+        Rectangle myRectangle = new Rectangle(2,2);
+        assertEquals(2, myRectangle.calcArea(), 0.0001);
+
+        myRectangle = new Rectangle(2,1);
+        assertEquals(2, myRectangle.calcArea(), 0.0001);
+        
+        myRectangle = new Rectangle(2,0);
+        assertEquals(2, myRectangle.calcArea(), 0.0001);
+    }
 }
