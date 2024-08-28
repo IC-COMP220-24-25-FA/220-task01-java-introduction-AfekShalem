d5cbe30e1206b53ac0eed665a912d77e52e17b8b
afeks <ashalemm@ithaca.edu>
2024-08-28 18:52:02 -0400

    longestLineWithinTest fixed and working

--- src/test/java/edu/ithaca/dragon/shapes/RectangleTest.java
+++ src/test/java/edu/ithaca/dragon/shapes/RectangleTest.java
@@ -19,12 +19,12 @@ public class RectangleTest {
     @Test
     public void longestLineWithinTest(){
         Rectangle myRectangle = new Rectangle(2,2);
-        assertEquals(2, myRectangle.calcArea(), 0.0001);
+        assertEquals(2, myRectangle.longestLineWithin(), 0.0001);
 
         myRectangle = new Rectangle(2,1);
-        assertEquals(2, myRectangle.calcArea(), 0.0001);
+        assertEquals(2, myRectangle.longestLineWithin(), 0.0001);
         
         myRectangle = new Rectangle(2,0);
-        assertEquals(2, myRectangle.calcArea(), 0.0001);
+        assertEquals(2, myRectangle.longestLineWithin(), 0.0001);
     }
 }
